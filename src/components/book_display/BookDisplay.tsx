b4f4229ed46854244de9b2a5685012c1f9f59f7f
ben-rieth <benrieth3@gmail.com>
2022-06-29 20:41:15 -0500

    Add shopping cart icon to corner

--- src/components/book_display/BookDisplay.tsx
+++ src/components/book_display/BookDisplay.tsx
@@ -1,4 +1,5 @@
 import Book from './../../services/types/Book';
+import AddToCartIcon from './../../assets/images/add_cart.svg';
 
 type BookProps = {
     book: Book;
@@ -7,10 +8,13 @@ type BookProps = {
 const BookDisplay = ({ book }: BookProps) => {
 
     return (
-        <div className="flex flex-col items-center outline outline-1 p-4 outline-slate-300">
+        <div className="flex flex-col items-center outline outline-1 p-4 outline-slate-300 relative">
             <img src={book.imageURL} alt="cover" className="w-24"/>
             <p className="font-bold">{book.title}</p>
             <p>${book.price}</p>
+            <div className="absolute top-0 right-0 hidden">
+                <img src={AddToCartIcon} alt="cart" className="w-8" />
+            </div>
         </div>
     )
 }
