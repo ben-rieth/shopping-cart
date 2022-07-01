f78dae9172e5882c3f2fba9936d36611dabbb0a9
ben-rieth <benrieth3@gmail.com>
2022-07-01 17:36:25 -0500

    Allows items in cart to have their quantity updated

--- src/App.tsx
+++ src/App.tsx
@@ -7,11 +7,32 @@ const App = () => {
 
     const [cart, setCart] = useState<Book[]>([]);
 
+    const addCartItem = (newItem: Book) => {
+
+        const itemAlreadyInCart = cart.find(item => item.id === newItem.id);
+        
+        if (itemAlreadyInCart) {
+            setCart(
+                cart.map((item) => {
+                    if (item.id === newItem.id) {
+                        item.quantity! += 1;
+                    }
+                    return item;
+                })
+            )
+        } else {
+            newItem.quantity = 1;
+            setCart(cart.concat(newItem))
+        }
+
+        console.log(cart);
+    }
+
     return (
         <CartContext.Provider 
             value={{
                 cart, 
-                addCartItem: (newItem: Book) => setCart(cart.concat(newItem)),
+                addCartItem,
                 removeCartItem: (itemId: string) => setCart(cart.filter((item) => item.id !== itemId))
             }}
         >
