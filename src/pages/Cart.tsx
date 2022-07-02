52081603f7749ebaa748d96d0ba92027c2e0af9c
ben-rieth <benrieth3@gmail.com>
2022-07-01 19:26:47 -0500

    Compute subtotal on cart page

--- src/pages/Cart.tsx
+++ src/pages/Cart.tsx
@@ -4,16 +4,22 @@ import { CartContext } from "../services/context/CartContext";
 import Header from './../components/header/Header';
 import BookGrid from './../components/book_grid/BookGrid';
 import Button from "../components/button/Button";
+import Book from "../services/types/Book";
 
 const Cart = () => {
     const { cart, addCartItem, removeCartItem} = useContext(CartContext);
 
+    const cartTotal : number = cart.reduce((prev: number, curr: Book) => prev + (curr.price * curr.quantity!), 0)
+
     return (
         <div>
             <Header />
-            <div className="flex justify-center items-center my-4">
+            <div className="flex flex-col justify-center items-center my-4">
                 {cart.length > 0 ?
-                    <Button text="Proceed to Checkout" />
+                    <>
+                        <h2>Subtotal: ${cartTotal}</h2>
+                        <Button text="Proceed to Checkout" />
+                    </>
                     : 
                     <h2 className="text-center">No Items in Your Cart</h2>
                 }
