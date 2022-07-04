9224f33159b9770ec399e1b3ab29516ee87a6f63
ben-rieth <benrieth3@gmail.com>
2022-07-03 21:15:05 -0500

    Add styles for empty cart

--- src/pages/Cart.tsx
+++ src/pages/Cart.tsx
@@ -6,6 +6,9 @@ import BookGrid from './../components/book_grid/BookGrid';
 import Button from "../components/button/Button";
 import Book from "../services/types/Book";
 
+import CartEmptyIcon from './../assets/images/cart.svg';
+import { Link } from "react-router-dom";
+
 const Cart = () => {
     const { cart } = useContext(CartContext);
 
@@ -14,14 +17,20 @@ const Cart = () => {
     return (
         <div>
             <Header />
-            <div className="flex flex-col justify-center items-center my-4">
+            <div className="flex flex-col justify-center items-center my-4 px-5 gap-2">
                 {cart.length > 0 ?
                     <>
                         <h2>Subtotal: ${cartTotal}</h2>
                         <Button text="Proceed to Checkout" />
                     </>
                     : 
-                    <h2 className="text-center">No Items in Your Cart</h2>
+                    <>
+                        <img src={CartEmptyIcon} alt="" className="w-28 h-28"/>
+                        <h2 className="text-center text-xl">There are currently no items in your shopping cart.</h2>
+                        <Link to="/browse">
+                            <Button text="Continue Shopping"/>
+                        </Link>
+                    </>
                 }
             </div> 
             <BookGrid books={cart} isCart={true}/>
