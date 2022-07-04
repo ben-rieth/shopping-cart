e3631652cf9ad58da910a726aed4ed6d97622f42
ben-rieth <benrieth3@gmail.com>
2022-07-03 21:21:24 -0500

    Change spacing on home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -13,8 +13,8 @@ function Home() {
   return (
     <div className="w-screen h-screen bg-home bg-center bg-cover">
       <Header />
-      <div className="flex flex-col items-center mt-48 py-5 bg-black/30">
-        <h2 className="text-center text-2xl text-white font-merienda">Ready to find a new favorite?</h2>
+      <div className="flex flex-col items-center gap-3 mt-48 py-5 bg-black/30">
+        <h2 className="text-center text-3xl text-white font-merienda">Ready to discover a new favorite?</h2>
         <Link to="/browse">
           <Button text="Browse"/>
         </Link>
