2246caa3fc80a8265c473ab080afa3e8f6a93cc0
ben-rieth <benrieth3@gmail.com>
2022-07-05 20:59:49 -0500

    Create seperate component for items in CartSidebar

--- src/components/cart_sidebar/CartSidebar.tsx
+++ src/components/cart_sidebar/CartSidebar.tsx
@@ -1,7 +1,7 @@
 import { useContext } from "react";
-import { Link } from "react-router-dom";
 import { CartContext } from "../../services/context/CartContext";
 import Book from "../../services/types/Book";
+import CartSidebarItem from "./CartSidebarItem";
 
 const CartSidebar = () => {
 
@@ -14,21 +14,11 @@ const CartSidebar = () => {
             <aside className="w-40 border-l-2 relative hidden xl:block">
                 <div className="absolute top-8 -left-[11px] w-0 h-0 border-y-[10px] border-y-transparent border-r-[10px] border-r-white"/>
                 <div className="flex flex-col items-center gap-5 h-screen py-5 overflow-y-auto">
-                    <div className="text-center">
+                    <div className="text-center" key="subtotal-sidebar">
                         <p>Subtotal</p>
                         <p className="font-bold">${cartTotal}</p>
                     </div>
-                    {cart.map((item: Book) => {
-                        return (
-                            <article className="relative" key={item.title}>
-                                <Link to={`/browse/${item.title}/${item.id}`}>
-                                    <img src={item.imageURL} alt="" className="w-24 cursor-pointer"/>
-                                    {item.quantity! > 1 ? 
-                                    <p className="absolute bottom-0 right-0 w-6 h-6 bg-red-700 text-center font-merienda">{item.quantity}</p> : ""}
-                                </Link>
-                            </article>
-                        )
-                    })}
+                    {cart.map((item: Book) => <CartSidebarItem book={item} key={`${item.title}-sidebar`}/>)}
                 </div>
             </aside>
         )
