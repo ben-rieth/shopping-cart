9cc6a09556feba6e7350196b04c3c4e1acc71427
ben-rieth <benrieth3@gmail.com>
2022-07-05 20:12:44 -0500

    Add tests for CartIcon

--- src/components/cart_icon/CartIcon.tsx
+++ src/components/cart_icon/CartIcon.tsx
@@ -4,15 +4,17 @@ import {CartContext} from './../../services/context/CartContext';
 import ShoppingBagIcon from './../../assets/images/shopping_bag.svg';
 
 const CartIcon = () => {
-    const cartContext = useContext(CartContext);
+    const {cart} = useContext(CartContext);
     const navigate = useNavigate();
 
-    const cartSize = cartContext.cart.length;
+    const cartSize = cart.length;
 
     return (
         <div className="relative" onClick={() => navigate("/cart")}>
             <img src={ShoppingBagIcon} alt="bag" className="cursor-pointer w-12 xl:w-16"/>
-            <p className="absolute top-3/4 left-1/2 -translate-x-1/2 -translate-y-3/4 text-center text-lg xl:text-3xl font-merienda pointer-events-none">{cartSize}</p>
+            <p className={`absolute top-3/4 left-1/2 -translate-x-1/2 -translate-y-3/4
+                            text-center text-lg font-merienda pointer-events-none xl:text-3xl
+                            ${cartSize === 0 ? 'hidden' : ''}`}>{cartSize}</p>
         </div>
     );
 }
