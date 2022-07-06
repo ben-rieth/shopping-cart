36636dcbbe7d9af8c4d669b586c22b08c8d05aa3
ben-rieth <benrieth3@gmail.com>
2022-07-05 20:43:23 -0500

    Make pictures in CartSidebar into links to their product page

--- src/components/cart_sidebar/CartSidebar.tsx
+++ src/components/cart_sidebar/CartSidebar.tsx
@@ -1,4 +1,5 @@
 import { useContext } from "react";
+import { Link } from "react-router-dom";
 import { CartContext } from "../../services/context/CartContext";
 import Book from "../../services/types/Book";
 
@@ -10,7 +11,7 @@ const CartSidebar = () => {
 
     if (cart.length > 0 ) {
         return (
-            <div className="w-40 border-l-2 relative hidden xl:block">
+            <aside className="w-40 border-l-2 relative hidden xl:block">
                 <div className="absolute top-8 -left-[11px] w-0 h-0 border-y-[10px] border-y-transparent border-r-[10px] border-r-white"/>
                 <div className="flex flex-col items-center gap-5 h-screen py-5 overflow-y-auto">
                     <div className="text-center">
@@ -19,15 +20,17 @@ const CartSidebar = () => {
                     </div>
                     {cart.map((item: Book) => {
                         return (
-                            <div className="relative">
-                                <img src={item.imageURL} alt="" className="w-24"/>
-                                {item.quantity! > 1 ? 
+                            <article className="relative" key={item.title}>
+                                <Link to={`/browse/${item.title}/${item.id}`}>
+                                    <img src={item.imageURL} alt="" className="w-24 cursor-pointer"/>
+                                    {item.quantity! > 1 ? 
                                     <p className="absolute bottom-0 right-0 w-6 h-6 bg-red-700 text-center font-merienda">{item.quantity}</p> : ""}
-                            </div>
+                                </Link>
+                            </article>
                         )
                     })}
                 </div>
-            </div>
+            </aside>
         )
     } else {
         return <div></div>;
