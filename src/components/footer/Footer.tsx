eb73cf20f95050c9e9850438532edbe6d59d3783
ben-rieth <benrieth3@gmail.com>
2022-07-08 16:43:50 -0500

    Add footer content

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,8 +1,12 @@
+import SocialMediaLink from "../social_media_link/SocialMediaLink";
+import GithubIcon from './../../assets/images/github.svg';
+
 const Footer = () => {
 
     return (
-        <div className="bg-slate-300 h-10 w-full shrink-0">
-
+        <div className="bg-slate-300 h-10 w-full shrink-0 flex justify-center items-center gap-3">
+            <p className="text-center font-merienda whitespace-pre-line">Cretaed by: Benji</p>
+            <SocialMediaLink linkTo="https://github.com/ben-rieth" imgUrl={GithubIcon} imgSize='sm'/>
         </div>
     )
 };
--- src/components/social_media_link/SocialMediaLink.tsx
+++ src/components/social_media_link/SocialMediaLink.tsx
@@ -3,12 +3,14 @@ import { Link } from "react-router-dom";
 type Props = {
     imgUrl: string;
     linkTo: string;
+    imgSize?: "sm" | "lg";
 }
 
-const SocialMediaLink = ({imgUrl, linkTo} : Props) => {
+const SocialMediaLink = ({imgUrl, linkTo, imgSize="lg"} : Props) => {
+
     return (
         <a href={linkTo}>
-            <img src={imgUrl} alt="" className="w-20 h-20 cursor-pointer hover:scale-110"/>
+            <img src={imgUrl} alt="" className={`${imgSize === 'lg' ? 'w-20 h-20' : 'w-5 h-5'} cursor-pointer hover:scale-110`}/>
         </a>
     )
 }
