89d120b84a3cd9f26f85dd795f0f77b4e87cd152
ben-rieth <benrieth3@gmail.com>
2022-07-08 16:54:23 -0500

    Change header bg color

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -8,12 +8,12 @@ const Header = () => {
 
     return (
         <div className="relative">
-            <div className="flex items-center justify-between px-3 py-4 bg-red-700 relative z-10">
+            <div className="flex items-center justify-between px-3 py-4 relative z-10 shadow-md bg-gradient-to-tl via-[#F2BF5E] to-[#B91C1C] from-[#F28705]">
                 <img src={MenuIcon} 
                     alt="menu" 
                     className="cursor-pointer w-12 xl:w-16" 
                     onClick={() => setSidebarVisible(true)}/>
-                <h1 className="text-2xl text-center font-merienda md:text-4xl xl:text-5xl">New Leaf Books</h1>
+                <h1 className="text-3xl text-center font-merienda font-semibold md:text-4xl xl:text-5xl text-transparent bg-clip-text bg-gradient-to-tl from-[#F28705] to-[#B91C1C]">New Leaf Books</h1>
                 <CartIcon />
             </div>
             <Sidebar isVisible={sidebarVisible} onCloseClick={() => setSidebarVisible(false)}/>
