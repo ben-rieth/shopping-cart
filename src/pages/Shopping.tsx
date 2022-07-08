7dd13f19c47a0ec7606a5e0a05634805a5546179
ben-rieth <benrieth3@gmail.com>
2022-07-08 14:43:11 -0500

    Sort by genre on main shop page

--- src/pages/Shopping.tsx
+++ src/pages/Shopping.tsx
@@ -3,8 +3,26 @@ import availableBooks from '../utils/AvailableBooks';
 import BookGrid from '../components/book_grid/BookGrid';
 import CartSidebar from "../components/cart_sidebar/CartSidebar";
 import LinkPath from "../components/link_path/LinkPath";
+import { ChangeEvent, useState } from "react";
+import Book from "../services/types/Book";
 
 const Shopping = () => {
+    const [selectedGenre, setSelectedGenre] = useState<string>("all");
+    const [bookList, setBookList] = useState<Book[]>(availableBooks);
+
+    const handleGenreChange = (event: ChangeEvent<HTMLSelectElement>) => {
+        const newGenre = event.target.value;
+        setSelectedGenre(newGenre);
+
+        if (newGenre === "all") {
+            setBookList(availableBooks);
+        } else {
+            setBookList(
+                availableBooks.filter((book) => book.genre.includes(newGenre))
+            );
+        }
+    }
+
     return (
         <div className="flex">
             <div className="w-screen">
@@ -14,7 +32,30 @@ const Shopping = () => {
                         links={[{to: '/', title: 'Home'}]} 
                         currentPage="Browse" />
                 </div>
-                <BookGrid books={availableBooks} />
+                <div>
+                    <label>
+                        Search By Genre:&nbsp;&nbsp;
+                        <select name="genre" value={selectedGenre} onChange={handleGenreChange}>
+                            <option value="all">Show All</option>
+                            <optgroup label="Genres">
+                                <option value="Absurdist">Absurdist</option>
+                                <option value="African-American">African American</option>
+                                <option value="Comedy">Comedy</option>
+                                <option value="Coming-of-Age">Coming Of Age</option>
+                                <option value="Dystopian">Dystopian</option>
+                                <option value="Historical">Historical</option>
+                                <option value="Modernist">Modernist</option>
+                                <option value="Realistic">Realistic</option>
+                                <option value="Romance">Romance</option>
+                                <option value="Satire">Satire</option>
+                                <option value="Social-Commentary">Social Commentary</option>
+                                <option value="Tragedy">Tragedy</option>
+                                <option value="War">War</option>
+                            </optgroup>
+                        </select>
+                    </label>
+                </div>
+                <BookGrid books={bookList} />
             </div>
             <CartSidebar />
         </div>
